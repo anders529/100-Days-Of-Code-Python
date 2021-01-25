694e6d88062d379104934488cd130c6ff7af9498
Aaron Andersen <aaronandersen@zohomail.com>
2021-01-25 00:11:53 -0600

    First Day of the challenge. Just the basics of Python for now.

---
+++ Day 1/exercise-3.py
@@ -0,0 +1,4 @@
+#Write a program that takes an input and displays how many characters there are in the name or word.
+#Write your code below this line 👇
+
+print( len( input("What is your name? ")))
